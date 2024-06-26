0d211ecde4fbbdf9a3ad7a76dba260e6fa5ac875
jaya verma <151820500+jayaverma21@users.noreply.github.com>
2024-06-26 12:18:10 +0530

    Update whatsapp.py

--- cgi-bin/whatsapp.py
+++ cgi-bin/whatsapp.py
@@ -18,9 +18,9 @@ message = form.getvalue('message')
 
 def send_whatsapp_message(phone_number, message):
     # Twilio credentials (replace with your actual credentials)
-    account_sid = 'your sid'
-    auth_token = 'here token'
-    twilio_whatsapp_number = 'whatsapp:'  # Twilio's sandbox number
+    account_sid = 'your twilio sid'
+    auth_token = 'your twilio auth token'
+    twilio_whatsapp_number = 'whatsapp:your twilio whatsapp no'  # Twilio's sandbox number
 
     client = Client(account_sid, auth_token)
     try:
